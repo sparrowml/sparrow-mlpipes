a9f6e19e3fec94cb0adbc502acfec64b838bf322
jbencook <ben@sparrow.dev>
2021-12-09 09:23:58 -0600

    more changes for function approach

--- mlpipes/bins/sink.py
+++ mlpipes/bins/sink.py
@@ -4,41 +4,17 @@ from mlpipes.initialize import Gst
 from mlpipes.element import make_element
 
 
-def make_sink_bin(
-    output_path: str,
-    visualization: bool = False,
-    osd_probe: Optional[
-        Callable[[Gst.Pad, Gst.PadProbeInfo, Tuple[Any, ...]], Gst.PadProbeReturn]
-    ] = None,
-) -> Gst.Bin:
+def make_sink_bin(output_path: str) -> Gst.Bin:
     bin = Gst.Bin.new("sink")
 
-    if visualization:
-        nvvideoconvert = make_element("nvvideoconvert")
-        Gst.Bin.add(bin, nvvideoconvert)
-
-        nvdsosd = make_element("nvdsosd")
-        Gst.Bin.add(bin, nvdsosd)
-        nvvideoconvert.link(nvdsosd)
-        if osd_probe:
-            nvdsosd.get_static_pad("sink").add_probe(
-                Gst.PadProbeType.BUFFER, osd_probe, 0
-            )
-
-        queue = make_element("queue")
-        Gst.Bin.add(bin, queue)
-        nvdsosd.link(queue)
-
-    nvvideoconvert2 = make_element("nvvideoconvert")
-    Gst.Bin.add(bin, nvvideoconvert2)
-    if visualization:
-        queue.link(nvvideoconvert2)
+    nvvideoconvert = make_element("nvvideoconvert")
+    Gst.Bin.add(bin, nvvideoconvert)
 
     capsfilter = make_element(
         "capsfilter", caps=Gst.Caps.from_string("video/x-raw, format=I420")
     )
     Gst.Bin.add(bin, capsfilter)
-    nvvideoconvert2.link(capsfilter)
+    nvvideoconvert.link(capsfilter)
 
     avenc_mpeg4 = make_element("avenc_mpeg4", bitrate=2000000)
     Gst.Bin.add(bin, avenc_mpeg4)
@@ -57,9 +33,6 @@ def make_sink_bin(
     Gst.Bin.add(bin, sink)
     qtmux.link(sink)
 
-    if visualization:
-        bin.add_pad(Gst.GhostPad("sink", nvvideoconvert.get_static_pad("sink")))
-    else:
-        bin.add_pad(Gst.GhostPad("sink", nvvideoconvert2.get_static_pad("sink")))
+    bin.add_pad(Gst.GhostPad("sink", nvvideoconvert2.get_static_pad("sink")))
 
     return bin
---
+++ mlpipes/bins/visualization.py
@@ -0,0 +1,32 @@
+from typing import Any, Callable, Optional, Tuple
+
+from mlpipes.initialize import Gst
+from mlpipes.element import make_element
+
+
+def make_visualization_bin(
+    osd_probe: Optional[
+        Callable[[Gst.Pad, Gst.PadProbeInfo, Tuple[Any, ...]], Gst.PadProbeReturn]
+    ] = None,
+) -> Gst.Bin:
+    bin = Gst.Bin.new("visualization")
+
+    nvvideoconvert = make_element("nvvideoconvert")
+    Gst.Bin.add(bin, nvvideoconvert)
+
+    nvdsosd = make_element("nvdsosd")
+    Gst.Bin.add(bin, nvdsosd)
+    nvvideoconvert.link(nvdsosd)
+    if osd_probe:
+        nvdsosd.get_static_pad("sink").add_probe(
+            Gst.PadProbeType.BUFFER, osd_probe, 0
+        )
+
+    queue = make_element("queue")
+    Gst.Bin.add(bin, queue)
+    nvdsosd.link(queue)
+
+    bin.add_pad(Gst.GhostPad("sink", nvvideoconvert.get_static_pad("sink")))
+    bin.add_pad(Gst.GhostPad("src", queue.get_static_pad("src")))
+
+    return bin
