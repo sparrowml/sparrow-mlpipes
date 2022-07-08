7ebfc9a6bb09d2929cf579c6e27069f027469cd1
jbencook <ben@sparrow.dev>
2022-07-08 13:35:08 +0000

    accept optional tracker config path

--- sparrow_mlpipes/bins/nvtracker.py
+++ sparrow_mlpipes/bins/nvtracker.py
@@ -1,10 +1,19 @@
+from pathlib import Path
+from typing import Optional, Union
+
 from sparrow_mlpipes.constants import DEEPSTREAM_DIRECTORY
 from sparrow_mlpipes.element import make_element
 from sparrow_mlpipes.initialize import Gst
 
 
-def make_nvtracker_bin() -> Gst.Bin:
+def make_nvtracker_bin(
+    tracker_config_path: Optional[Union[Path, str]] = None
+) -> Gst.Bin:
     """Create and configure nvtracker element."""
-    ll_lib_file = DEEPSTREAM_DIRECTORY / "lib/libnvds_nvmultiobjecttracker.so"
-    nvtracker = make_element("nvtracker", ll_lib_file=str(ll_lib_file))
+    kwargs = dict(
+        ll_lib_file=str(DEEPSTREAM_DIRECTORY / "lib/libnvds_nvmultiobjecttracker.so")
+    )
+    if tracker_config_path:
+        kwargs["ll_config_file"] = str(tracker_config_path)
+    nvtracker = make_element("nvtracker", **kwargs)
     return nvtracker
