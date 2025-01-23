38757b0a458d758cb2b8070e7b57569fa21423de
jbencook <ben@sparrow.dev>
2025-01-23 16:32:23 +0000

    fix post create command

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,4 +2,4 @@ pip install -r requirements-dev.txt
 mkdir -p /home/${USER}/.ssh
 sudo cp -r /home/${USER}/ssh/. /home/${USER}/.ssh
 sudo chown -R ${USER} /home/${USER}/.ssh
-sudo chmod -R 755 .git
+sudo chown -R ${USER} .git
